28251426205517771b63fcc3a2956b52b0600beb
antblessing <antblessing@gmail.com>
2016-07-09 00:24:04 -0700

    add homepage template and styling

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,17 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+ChessClass.delete_all
+Instructor.delete_all
+
+
+ChessClass.create!(school: "Indian Valley", size: 30, price: 100, days: "Thursday")
+ChessClass.create!(school: "Walnut Acres", size: 21, price: 100, days: "Monday")
+ChessClass.create!(school: "Baldwin", size: 40, price: 100, days: "Friday")
+ChessClass.create!(school: "Stoneman", size: 10, price: 100, days: "Wednesday")
+ChessClass.create!(school: "Highlands", size: 15, price: 100, days: "Tuesday")
+
+Instructor.create!(first_name: "Anthony", last_name: "Blessing", email: "antblessing@gmail.com")
+Instructor.create!(first_name: "Jeff", last_name: "McCann", email: "jeff@gmail.com")
+Instructor.create!(first_name: "Ryan", last_name: "Blessing", email: "ryan@gmail.com")
+Instructor.create!(first_name: "Socks", last_name: "Cat", email: "socks@gmail.com")
+
+puts "#{ChessClass.count} classes created."
+puts "#{Instructor.count} instructors created."
